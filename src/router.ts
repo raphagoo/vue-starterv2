cdb6c684bac95c323402b086907cb9cc32663474
Raphaël Agostini <raphael.agostini@takoma.fr>
2022-08-16 15:45:49 +0200

    update packages

--- src/router.ts
+++ src/router.ts
@@ -18,7 +18,7 @@ export const router = createRouter({
     ]
 })
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to: any, from: any, next: any) => {
     // redirect to login page if not logged in and trying to access a restricted page
     //const publicPages = ['/login', '/register', '/', '/shop', '/product', '/about', '/products'];
     //const authRequired = !publicPages.includes(to.path);
